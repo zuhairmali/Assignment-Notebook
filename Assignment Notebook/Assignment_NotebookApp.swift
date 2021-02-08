e02fb9ab19b2c68b0141bab8cbf98b8abe6ec1ec
Zuhair Ali <zuhairali20003@gmail.com>
2021-02-08 10:28:02 -0600

    Initial Commit

---
+++ Assignment Notebook/Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NotebookApp.swift
+//  Assignment Notebook
+//
+//  Created by Student on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
